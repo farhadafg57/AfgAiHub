8d28fa7be681d37a7991998ac1f9bf9094f6c232
Farhad Sakhi <forextraderafg@gmail.com>
2025-10-28 15:09:12 +0000

    I am getting 404 error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,7 +65,7 @@ export default async function RootLayout({
       )}
     >
       <body className="font-sans antialiased">
-        <NextIntlClientProvider locale={locale} messages={messages}>
+        <NextIntlClientProvider messages={messages}>
           <FirebaseProvider>{children}</FirebaseProvider>
         </NextIntlClientProvider>
       </body>
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,14 @@ import createMiddleware from 'next-intl/middleware';
 export default createMiddleware({
   // A list of all locales that are supported
   locales: ['en', 'fa', 'ps'],
-
   // Used when no locale matches
   defaultLocale: 'en',
+  // All public pages that need translations
+  localePrefix: 'always',
 });
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(fa|ps|en)/:path*'],
+  // Skip all paths that should not be internationalized
+  // This includes the dashboard, API routes, and static assets.
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|dashboard).*)'],
 };
