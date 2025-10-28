88eba1f15e3b0ba8860854266a54f43e84636c64
Farhad Sakhi <forextraderafg@gmail.com>
2025-10-28 11:52:32 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -10,15 +10,16 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'i.pravatar.cc',
-      }
+        port: '',
+        pathname: '/**',
+      },
     ],
-  },
-  compiler: {
-    reactRemoveProperties: true,
   },
 };
 
